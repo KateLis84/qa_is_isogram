e36383bd542a5532a0dee9b296524eeae14e4de8
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-08-07 21:20:33 +0300

    finish the task

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,21 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  // eslint-disable-next-line max-len
+  it('returns true for a word with no repeated letters like "background"', () => {
+    expect(isIsogram('background')).toBeTruthy();
+  });
+
+  it('returns false for a word with duplicate letters like "tree"', () => {
+    expect(isIsogram('tree')).toBeFalsy();
+  });
+
+  it('returns true for an empty string input', () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it('returns false if the word has repeated letters, e.g. "Apple"', () => {
+    expect(isIsogram('Apple')).toBeFalsy();
+  });
 });
